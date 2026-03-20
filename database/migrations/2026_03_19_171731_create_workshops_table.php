2a90baba2bf6e43f0aa7da50e9dc50295e9dc150
anxietyPotato <krompirko558@gmail.com>
2026-03-20 01:18:35 +0100

    feat: add role to users, workshops and registrations migrations

---
+++ database/migrations/2026_03_19_171731_create_workshops_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workshops', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('starts_at');
+            $table->dateTime('ends_at');
+            $table->unsignedInteger('capacity');
+            $table->foreignId('created_by')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workshops');
+    }
+};
